abc9eabea3d5d86de1f8a4377c45983a293d01a4
DEBargha2004 <debarghasaha16@gmail.com>
2024-02-24 23:06:30 +0530

    fix
    message image component seperated

--- src/components/custom/MessageComponent.tsx
+++ src/components/custom/MessageComponent.tsx
@@ -4,6 +4,7 @@ import Image from 'next/image'
 import { useEffect, useState } from 'react'
 import { bucket } from '../../../firebase.config'
 import { Dialog, DialogContent, DialogTrigger } from '../ui/dialog'
+import MessageImage from './MessageImage'
 
 export default function MessageComponent ({ type, data }: MessageComponentType) {
   switch (type) {
@@ -15,35 +16,4 @@ export default function MessageComponent ({ type, data }: MessageComponentType) 
       return <MessageImage data={data} />
       break
   }
-}
-
-export function MessageImage ({ data }: { data: string }) {
-  const [url, setUrl] = useState<string>('')
-  useEffect(() => {
-    getDownloadURL(ref(bucket, `images/${data}`)).then(url => setUrl(url))
-  }, [data])
-  return (
-    <Dialog>
-      <DialogTrigger asChild>
-        <div className='w-[100px] h-[100px] overflow-hidden rounded'>
-          <Image
-            alt='image'
-            src={url}
-            width={100}
-            height={100}
-            className='w-full cursor-pointer object-cover h-full'
-          />
-        </div>
-      </DialogTrigger>
-      <DialogContent>
-        <Image
-          alt='image'
-          src={url}
-          width={300}
-          height={300}
-          className='w-full  rounded'
-        />
-      </DialogContent>
-    </Dialog>
-  )
 }
